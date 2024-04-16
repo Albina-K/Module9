4477dedf75946873c7eccc0bc32f5c7eb96083da
Albina-K <kamalieva.albish@mail.ru>
2024-04-16 21:14:23 +0300

    процеесс

--- Module9/Program.cs
+++ Module9/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using static Module9.Program7;
 
 namespace Module9
 {
@@ -284,7 +286,7 @@ namespace Module9
             Console.ReadKey();
         }
 
-        ///or
+        //or
         public delegate string GreetingsDelegate(string name);
         static void Main(string[] args)
         {
@@ -297,7 +299,7 @@ namespace Module9
             Console.ReadKey();
         }
 
-        /// or        
+        // or        
         public class AnonymousMethods
         {
             public delegate string GreetingsDelegate(string name);
@@ -315,4 +317,231 @@ namespace Module9
             }
         }
     }
-}
+    public class Program7
+    {
+        public delegate bool EligibleToPromotion(Employee EmployeeToPromotion);
+        static void Main(string[] args)
+        {
+            Employee empl1 = new Employee()
+            {
+                ID = 55,
+                Name = "Alexey",
+                Experience = 5,
+                Salary = 20000
+            };
+            Employee empl2 = new Employee()
+            {
+                ID = 545,
+                Name = "A4exey",
+                Experience = 45,
+                Salary = 20400
+            };
+            Employee empl3 = new Employee()
+            {
+                ID = 543,
+                Name = "A4ex3ey",
+                Experience = 435,
+                Salary = 203400
+            };
+
+            List<Employee> lstEmployees = new List<Employee>();///объявили пустой список сотрудников
+            lstEmployees.Add(empl1);///добавляем в пустой список созданных ранее сотрудников
+            lstEmployees.Add(empl2);
+            lstEmployees.Add(empl3);
+
+            EligibleToPromotion eligibleToPromotion = Promote; ///в наш делегат присваеваем метод Promote
+            Employee.PromoteEmpoyee(lstEmployees, eligibleToPromotion);///вызываем метод PromoteEmpoyee, на вход ожидает список сотрудников который мы уже сделали, второй паарметр это делегат, котрый мы только что объявили
+            Console.ReadKey();
+        }
+        public static bool Promote(Employee employee) ///возвращает значение подходит ли сотрудник под условие повышения
+        {
+            if (employee.Salary > 10000) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+    // класс сотрудников  
+    
+    public class Employee
+    {
+        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Experience { get; set; }
+        public int Salary { get; set; }
+
+        //метод выводит повышенных сотрудников
+        public static void PromoteEmpoyee(List<Employee> listEmployees, EligibleToPromotion IsEmploteeEligible)//на вход требует список сотрудников и воторой параметр это делегат
+        {
+            ///цикл перебор каждого сотрудника из входного списка
+            foreach(Employee employee in listEmployees) 
+            {
+                if (IsEmploteeEligible(employee))///условие, если попадает под условие повышения мы выводим данного сотрудника в консоль
+                {
+                    Console.WriteLine("Employee {0} Promoted,", employee.Name);
+                }
+            }
+        }
+    }
+
+    //лямбда оператор во время анонимного метода
+    class Program8
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = (string _message) =>
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
+        }
+
+        static void ShowMessage(string _message)
+        {
+            Console.WriteLine(_message);
+        }
+    }
+
+    class Program8_0///то что было до лямбда оператора
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
+        }
+
+        static void ShowMessage(string _message)
+        {
+            Console.WriteLine(_message);
+        }
+    }
+
+    //лямбда оператор во время анонимного метода
+    class Program9
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = () =>
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+
+    class Program9_0///было до лямбда оператора
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = RandomNumber;
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        static int RandomNumber()
+        {
+            return new Random().Next(0, 100);
+        }
+    }
+    //ковариативность это когда мы можем объявлять делегат и назначать ему методы другой сигнатуры, но которые являются производными от основного метода.
+    class Car
+    {
+        public string Model { get; set; }
+    }
+    class BMW : Car 
+    { 
+    
+    }
+    class Program10
+    {
+        delegate Car CarDelegate(string name);
+        static void Main(string[] args)
+        {
+            CarDelegate carDelegate;
+            carDelegate = BuildBMW; // ковариантность
+            Car c = carDelegate("X6");
+            Console.WriteLine(c.Model);
+            Console.Read();
+        }
+        private static BMW BuildBMW(string model)
+        {
+            return new BMW { Model = model };
+        }
+    }
+    public class Program11
+    {
+        delegate void BwmInfo(BMW bwm);
+        static void Main(string[] args)
+        {
+            BwmInfo bmwInfo = GetCarInfo; // контравариантность
+            BMW bwm = new BMW
+            {
+                Model = "X6"
+            };
+            bmwInfo(bwm);
+            Console.Read();
+        }
+
+        private static void GetCarInfo(Car p)
+        {
+            Console.WriteLine(p.Model);
+        }
+    }
+
+    class Program12
+    {
+        public delegate Animal HandLerMethod(); //делегат имеем класс возращаемого знаечия энимал
+
+        public delegate void DogInfo(Dog dog); //на вход класс дог
+        public static Animal AnimalHandler()
+        {
+            return null;
+        }
+
+        public static Dog DogHandler()
+        {
+            return null;
+        }
+        static void Main(string[] args) 
+        {
+            DogInfo dog = GetAnimalInfo;
+            dog.Invoke(new Dog());//через метод dog.Invoke присваиваем объект класса
+
+            HandLerMethod handLerMethod = AnimalHandler;
+
+            HandLerMethod handLerDog = DogHandler;
+
+            Console.Read();
+        }
+
+        public static void GetAnimalInfo(Animal p)
+        {
+            Console.WriteLine(p.GetType()); //в консольное приложение выведится тип нашего класса который поступил в метод GetAnimalInfo
+        }
+    }
+
+    class Animal
+    {
+
+    }
+
+    class Dog : Animal
+    {
+
+    }
